0e76acba5452a1ea065aaf41503b5fbb15f32ce1
Rafael Rocha <rafaelcosta@cloudfox.net>
2021-10-22 10:20:31 -0300

    refactor: move level to its own repository

--- src/LevelRepositoryMemory.ts
+++ src/LevelRepositoryMemory.ts
@@ -1,20 +1,24 @@
 import LevelRepository from "./LevelRepository";
 
 export default class LevelRepositoryMemory implements LevelRepository {
-  levels: any = [
-    {
-      code: "EF1",
-      description: "Ensino Fundamental I",
-    },
-    {
-      code: "EF2",
-      description: "Ensino Fundamental II",
-    },
-    {
-      code: "EM",
-      description: "Ensino Médio",
-    },
-  ];
+  levels: any[];
+
+  constructor() {
+    this.levels = [
+      {
+        code: "EF1",
+        description: "Ensino Fundamental I",
+      },
+      {
+        code: "EF2",
+        description: "Ensino Fundamental II",
+      },
+      {
+        code: "EM",
+        description: "Ensino Médio",
+      },
+    ];
+  }
 
   findByCode(code: string): any {
     const level = this.levels.find((lvl: any) => lvl.code === code);
